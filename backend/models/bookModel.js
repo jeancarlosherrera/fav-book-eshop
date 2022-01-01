5c87f8fc20f28c6b7e6d702f130ded0504e54338
Jean C. Herrera <jeancarlosherreram@gmail.com>
2022-01-01 18:29:42 -0500

    mongoose schemas defined

---
+++ backend/models/bookModel.js
@@ -0,0 +1,81 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const bookSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    pages: {
+      type: Number,
+      required: true,
+    },
+    firstEdition: {
+      type: Number,
+      required: true,
+    },
+    synopsis: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Book = mongoose.model('Book', bookSchema);
+
+export default Book;

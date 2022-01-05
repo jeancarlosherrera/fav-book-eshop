d9f78149ddc0bd02ec574d51ea65281354fa2eef
Jean C. Herrera <jeancarlosherreram@gmail.com>
2022-01-05 13:38:14 -0500

    redux setup

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,35 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Book from '../components/Book';
-import axios from 'axios';
+import { listBooks } from '../actions/bookActions';
 
 const HomeScreen = () => {
-  const [books, setBooks] = useState([]);
+  const dispatch = useDispatch();
+
+  const bookList = useSelector((state) => state.bookList);
+  const { loading, error, books } = bookList;
 
   useEffect(() => {
-    const fetchBooks = async () => {
-      const { data } = await axios.get('/api/books');
-      setBooks(data);
-    };
-    fetchBooks();
-  }, []);
-
-  // useEffect(() => {
-  //   axios.get('/api/books').then((res) => {
-  //     setBooks(res.data);
-  //   });
-  // }, []);
+    dispatch(listBooks());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Featured Books</h1>
-      <Row>
-        {books.map((book) => (
-          <Col key={book._id} sm={12} md={6} lg={4} xl={3}>
-            <Book book={book} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {books.map((book) => (
+            <Col key={book._id} sm={12} md={6} lg={4} xl={3}>
+              <Book book={book} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };

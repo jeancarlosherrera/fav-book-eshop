a90743ffe9f01973add760b2d7b80dff49ed095f
Jean C. Herrera <jeancarlosherreram@gmail.com>
2022-01-08 12:18:20 -0500

    auth middleware implemented

--- backend/controllers/bookControllers.js
+++ backend/controllers/bookControllers.js
@@ -1,11 +1,13 @@
 import asyncHandler from 'express-async-handler';
 import Book from '../models/bookModel.js';
 
+// Get all books - GET /api/books
 const getBooks = asyncHandler(async (req, res) => {
   const allBooks = await Book.find({});
   res.json(allBooks);
 });
 
+// Get individual book - GET /api/books/:id
 const getBookById = asyncHandler(async (req, res) => {
   const selectedBook = await Book.findById(req.params.id);
   if (selectedBook) {
--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,6 +2,7 @@ import asyncHandler from 'express-async-handler';
 import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
 
+// Auth user - POST /api/users/login
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
@@ -19,4 +20,20 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// Get user profile - GET /api/users/profile
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, getUserProfile };

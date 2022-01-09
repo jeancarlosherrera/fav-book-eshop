6cdd352ceb2e722f53df441046ad91a4e5a93dd2
Jean C. Herrera <jeancarlosherreram@gmail.com>
2022-01-09 17:50:28 -0500

    login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import BookScreen from './screens/BookScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Container>
           <Routes>
             <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/login' element={<LoginScreen />} />
             <Route path='/book/:id' element={<BookScreen />} />
             <Route path='/cart'>
               <Route path=':id' element={<CartScreen />} />

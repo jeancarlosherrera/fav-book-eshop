87b6e38784cc97a9cfc423be142c70a42c9852b4
Jean C. Herrera <jeancarlosherreram@gmail.com>
2022-01-10 18:07:17 -0500

    added user registration functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import HomeScreen from './screens/HomeScreen';
 import BookScreen from './screens/BookScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Routes>
             <Route path='/' element={<HomeScreen />} exact />
             <Route path='/login' element={<LoginScreen />} />
+            <Route path='/register' element={<RegisterScreen />} />
             <Route path='/book/:id' element={<BookScreen />} />
             <Route path='/cart'>
               <Route path=':id' element={<CartScreen />} />

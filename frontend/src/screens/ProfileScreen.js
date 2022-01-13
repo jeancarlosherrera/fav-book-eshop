ee76d085119d6e0079df9264af8be5bd85cce140
Jean C. Herrera <jeancarlosherreram@gmail.com>
2022-01-13 17:12:52 -0500

    update user functionality

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -4,7 +4,7 @@ import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { register, getUserDetails } from '../actions/userActions';
+import { getUserDetails, updateUserPofile } from '../actions/userActions';
 
 const ProfileScreen = () => {
   const [name, setName] = useState('');
@@ -21,6 +21,9 @@ const ProfileScreen = () => {
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const { success } = userUpdateProfile;
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -34,16 +37,15 @@ const ProfileScreen = () => {
         setEmail(user.email);
       }
     }
-  }, [navigate, userInfo, dispatch, user.name, user.email]);
+  }, [navigate, userInfo, dispatch, user]);
 
   const submitHandler = (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
       setMessage('Passwords do not match');
     } else {
-      // ????
+      dispatch(updateUserPofile({ id: user._id, name, email, password }));
     }
-    dispatch(register(name, email, password));
   };
   return (
     <Row>
@@ -51,6 +53,7 @@ const ProfileScreen = () => {
         <h2>User Profile</h2>
         {message && <Message variant='danger'>{message}</Message>}
         {error && <Message variant='danger'>{error}</Message>}
+        {success && <Message variant='success'>Profile Updated!</Message>}
         {loading && <Loader />}
         <Form onSubmit={submitHandler}>
           <Form.Group controlId='name'>

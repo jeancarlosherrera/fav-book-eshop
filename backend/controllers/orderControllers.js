07e4d9ed3dd5d172b2b7d982c9efe780eb8e4430
Jean C. Herrera <jeancarlosherreram@gmail.com>
2022-01-24 20:35:44 -0500

    order by id route

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
 
+// Create new order - POST /api/orders
 const addOrderItems = asyncHandler(async (req, res) => {
   const {
     orderItems,
@@ -27,11 +28,24 @@ const addOrderItems = asyncHandler(async (req, res) => {
       shippingPrice,
       totalPrice,
     });
-
     const createdOrder = await order.save();
-
     res.status(201).json(createdOrder);
   }
 });
 
-export { addOrderItems };
+// Get order by ID - GET /api/orders/:id
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
+export { addOrderItems, getOrderById };
